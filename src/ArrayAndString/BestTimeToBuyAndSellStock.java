8769877bdfbe0a8c896615d951d9b84cd85e1c5c
Helena M <markulinh@gmail.com>
2024-12-12 13:48:33 +0100

    Big-O Cheatsheet

--- src/ArrayAndString/BestTimeToBuyAndSellStock.java
+++ src/ArrayAndString/BestTimeToBuyAndSellStock.java
@@ -1,21 +1,43 @@
+import java.util.Arrays;
+
+
 public class BestTimeToBuyAndSellStock {
 
 
     public static int maxProfit(int[] prices) {;
-        int profit = 0;
+        // int profit = 0;
 
-        for (int i = 0; i < prices.length-1 ; i++){
-            for (int j = i + 1; j < prices.length ; j++){
-                if(prices[i] - prices[j] < profit){
-                    profit = prices[i] - prices[j];
-                }
+        // for (int i = 0; i < prices.length-1 ; i++){
+        //     for (int j = i + 1; j < prices.length ; j++){
+        //         if(prices[i] - prices[j] < profit){
+        //             profit = prices[i] - prices[j];
+        //         }
+        //     }
+        // }
+        // return Math.abs(profit);
+        int bestBuyDay = 0;
+        int bestSellDay = 0;
+        int bestBuyDayIndex = 0;
+ 
+
+        for(int i = 0; i < prices.length - 1; i++){
+            if(prices[i] < bestBuyDay){
+                bestBuyDay = prices[i];
             }
         }
-        return Math.abs(profit);
+
+        for(int i = 0; i < prices.length; i++){
+
+        }
+
+
+        
+
+
     }
 
     public static void main(String[] args) {
-        int [] prices = {7,1,5,3,6,4};
+        int [] prices = {3,2,6,5,0,3};
         System.out.println(maxProfit(prices));
     }
 }
